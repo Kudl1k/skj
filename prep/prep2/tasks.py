45545418c6eb7d720563638d7f8732416a26284e
Kudl1k <stepanek.kudlacek@gmail.com>
2024-03-18 22:33:07 +0100

    done

---
+++ prep/prep2/tasks.py
@@ -0,0 +1,52 @@
+import xml.etree.ElementTree as ET
+
+def create_student(xml_root, student_id):
+    '''
+    Vytvořte studenta dle loginu.
+    Ujistěte se, že student neexistuje, jinak: raise Exception('student already exists')
+    '''
+    for student in xml_root.findall('student'):
+        if student.get('student_id') == student_id:
+            raise Exception('student already exists')
+    ET.SubElement(xml_root, 'student', {'student_id': student_id})
+
+def remove_student(xml_root, student_id):
+    '''
+    Odstraňte studenta dle loginu
+    '''
+    for student in xml_root.findall('student'):
+        if student.get('student_id') == student_id:
+            xml_root.remove(student)
+            return
+
+def set_task_points(xml_root, student_id, task_id, points):
+    '''
+    Přepište body danému studentovi u jednoho tasku
+    '''
+    for student in xml_root.findall('student'):
+        if student.get('student_id') == student_id:
+            for task in student.findall('task'):
+                if task.get('task_id') == task_id:
+                    task.text = str(points)
+                    return
+
+def remove_task(xml_root, task_id):
+    '''
+    Napříč všemi studenty smažte task s daným task_id
+    '''
+    for student in xml_root.findall('student'):
+        for task in student.findall('task'):
+            if task.get('task_id') == task_id:
+                student.remove(task)
+                return
+                
+
+def create_task(xml_root, student_id, task_id, points):
+    for student in xml_root.findall('student'):
+        if student.get('student_id') == student_id:
+            for task in student.findall('task'):
+                if task.get('task_id') == task_id:
+                    raise Exception('task already exists')
+            new_task = ET.SubElement(student, 'task', {'task_id': task_id, 'points': str(points)})
+            new_task.text = str(points)
+            return
