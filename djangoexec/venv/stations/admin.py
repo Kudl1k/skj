33b1b86f57ae1137c8235bb65e361dfcdc1acc4b
Kudl1k <stepanek.kudlacek@gmail.com>
2024-04-16 12:04:05 +0200

    django

---
+++ djangoexec/venv/stations/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Station,Company
+
+# Register your models here.
+
+admin.site.register(Station)
+admin.site.register(Company)
+
---
+++ djangoexec/venv/stations/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Station(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=150)
+    active = models.BooleanField(default=False)
+    created = models.DateTimeField('date created',auto_now_add=True)
+    cars_capacity = models.IntegerField
+    Company = models.ForeignKey('Company',null=True,blank=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} - {self.address}'    
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
