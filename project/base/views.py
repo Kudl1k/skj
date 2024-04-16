c8257a115b0cad169b20f80631959bb88ecf525c
Kudl1k <stepanek.kudlacek@gmail.com>
2024-04-16 15:41:42 +0200

    create an app and setup the urls and views

---
+++ project/base/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+rooms = [
+    {'id': 1, 'name' : 'Room 1'},
+    {'id': 2, 'name' : 'Room 2'},
+    {'id': 3, 'name' : 'Room 3'}
+]
+
+
+def home(request):
+    return render(request, 'home.html', {'rooms': rooms})
