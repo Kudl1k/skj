b70a97cf5b86b17ed3d31c60f6766bcb79a24e03
Kudl1k <stepanek.kudlacek@gmail.com>
2024-04-24 00:30:14 +0200

    fix some bugs in the result and add player for the football category

--- project/base/views.py
+++ project/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from .models import League, Category, Match
+from django.db.models import Q
+from django.utils import timezone
+from .models import League, Category, Match, Team, Player, PlayerHistory
 
 # Create your views here.
 
@@ -22,15 +24,21 @@ def home(request, league_id=None, match_id=None):
         'leagues': leagues,
         'matches': matches,
         'selected_match': match,
-        'title': 'Football Page',
+        'title': 'Football Results',
+        'type_view': 'results',
     }
     return render(request, 'base/home.html', context)
 
 def hockey(request, league_id=None,match_id= None):
+    match = None
     category = Category.objects.get(name='Ice Hockey')
     leagues = League.objects.filter(id_category=category)
     if league_id:
         league = League.objects.get(id=league_id)
+        if match_id:
+            match = Match.objects.get(id=match_id)
+        else:
+            match = None
         matches = Match.objects.filter(id_league=league)
     else:
         matches = None
@@ -38,15 +46,22 @@ def hockey(request, league_id=None,match_id= None):
     context = {
         'leagues': leagues,
         'matches': matches,
-        'title': 'Football Page',
+        'selected_match': match,
+        'title': 'Ice Hockey Results',
+        'type_view': 'results',
     }
     return render(request, 'base/home.html', context)
 
 def basketball(request, league_id=None,match_id= None):
+    match = None
     category = Category.objects.get(name='Basketball')
     leagues = League.objects.filter(id_category=category)
     if league_id:
         league = League.objects.get(id=league_id)
+        if match_id:
+            match = Match.objects.get(id=match_id)
+        else:
+            match = None
         matches = Match.objects.filter(id_league=league)
     else:
         matches = None
@@ -54,7 +69,9 @@ def basketball(request, league_id=None,match_id= None):
     context = {
         'leagues': leagues,
         'matches': matches,
-        'title': 'Football Page',
+        'selected_match': match,
+        'title': 'Basketball Results',
+        'type_view': 'results',
     }
     return render(request, 'base/home.html', context)
 
@@ -64,5 +81,27 @@ def login(request):
 def signup(request):
     return render(request, 'base/singup.html')
 
-def players(request):
-    return render(request, 'base/players.html')
+def players_football(request, league_id=None, team_id=None, player_id = None):
+    category = Category.objects.get(name='Football')
+    leagues = League.objects.filter(id_category=category)
+    teams = None
+    players = None
+    player_info = None
+    if league_id:
+        teams = Team.objects.filter(id_league=league_id)
+        if team_id:
+            player_histories = PlayerHistory.objects.filter(Q(id_team=team_id) & (Q(end_date__isnull=True) | Q(end_date__gte=timezone.now())))
+            players = [{'player': history.id_player, 'history': history} for history in player_histories]
+            if player_id:
+                player = Player.objects.get(id=player_id)
+                player_history = PlayerHistory.objects.filter(id_player=player_id)
+                player_info = {'player': player, 'history': player_history}
+    context = {
+        'leagues': leagues,
+        'teams': teams,
+        'players': players,
+        'player_info': player_info,
+        'title': 'Football Players',
+        'type_view': 'players'
+    }
+    return render(request, 'base/players.html', context)
